aa795668a020cb38482be3bedf42e28caaeeae18
Sell Cringe <sellcringe@yandex.ru>
2023-05-23 00:31:27 +0400

    second commit from Render deploy

--- database.js
+++ database.js
@@ -3,12 +3,34 @@ require('dotenv').config()
 
 const database = {
     dialect: 'postgres',
-    host: process.env.POSTGRES_HOST,
-    port: 5432,
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB
+    client: 'pg',
+    url: process.env.DATABASE_URL,
+    protocol: 'postgres',
+    // host: process.env.POSTGRES_HOST,
+    // port: 5432,
+    // username: process.env.POSTGRES_USER,
+    // password: process.env.POSTGRES_PASSWORD,
+    // database: process.env.POSTGRES_DB,
+
+
+    ssl: true,
+    native: true
+
+
 }
+
+// const knex = require('knex');
+// const db = knex({
+//     client: 'pg',
+//     connection: {
+//         connectionString: 'postgres://apihhru_user:QCb3OXlaXyrRhzNygiMhOauWS5kT7d96@dpg-chlmre64dadfmsgj44mg-a/apihhru',
+//         ssl: {
+//             rejectUnauthorized: false
+//         }
+//     }
+// });
+
+// module.exports = db;
 
 const sequelize = new Sequelize(database)
 
@@ -24,6 +46,5 @@ db.User.hasMany(db.Post, {
     onUpdate: 'CASCADE'
 })
 db.Post.belongsTo(db.User)
-
 
 module.exports = db
